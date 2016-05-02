c9fb1d1bd3fdebb47a7ea37bbb6bbafc0a0899d5
Lyka <flowerch@users.noreply.github.com>
2016-05-02 04:07:47 +0300

    Update tournament.sql

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -1,56 +1,34 @@
--- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
---
--- Clear out any previous tournament databases.
-
+-- Clear out any previous DB.
 DROP DATABASE IF EXISTS tournament;
 
--- Create database.
-
+-- Create DB.
 CREATE DATABASE tournament;
 
 -- Connect to the DB before creating tables.
-
-\c tournament;
+\c
+ tournament;
 
 -- Create table for players.
-
 CREATE TABLE players (
-id serial primary key, 
-name text
+id serial PRIMARY KEY,
+name text NOT NULL
 );
 
 -- Create table for games.
-
 CREATE TABLE matches(
-game_id serial primary key, 
-winner integer REFERENCES players(id), 
-loser integer REFERENCES players(id), 
+id serial PRIMARY KEY,
+winner INT REFERENCES players(id) ON DELETE CASCADE,
+loser INT REFERENCES players(id) ON DELETE CASCADE
 );
 
--- Create view to show standings. 
-
+-- Create view to show standings.
 CREATE VIEW standings AS
-SELECT players.id, 
+SELECT players.id,
 players.name,
 COUNT(matches.winner = players.id) AS wins,
 COUNT(matches.*) AS games
 FROM players LEFT JOIN matches
-ON players.id = matches.winner OR 
+ON players.id = matches.winner OR
 players.id = matches.loser
-GROUP BY players.id;
-
---Create a view for number of byes per player
-
-CREATE VIEW byes AS
-SELECT players.id,
-players.name,
-COUNT (matches.winner) AS bye_count
-FROM players LEFT JOIN matches
-ON playes.id = matches.winner AND matches.loser = NULL
-GROUP BY players.id;
+GROUP BY players.id, players.name
+ORDER BY wins DESC;
