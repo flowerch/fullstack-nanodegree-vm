24986983ed6ba727535bace65decb01deb7a1ef1
Lyka <flowerch@users.noreply.github.com>
2016-05-02 03:57:29 +0300

    Update tournament.py

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -3,44 +3,51 @@
 # tournament.py -- implementation of a Swiss-system tournament
 #
 import psycopg2
-
-from psycopg2 import connect
-
+from psycopg2._psycopg import Error, Warning, DataError, DatabaseError, ProgrammingError
+import psycopg2.extensions
 import itertools
-
-import pdb
+    
 
 def connect(database_name="tournament"):
     """Connect to the PostgreSQL database.  Returns a database connection."""
+    # We make use of the connect() method so that we could avoid the code repetition.
+    # We can refactor our connect() method to deal not only with the database connection
+    # but also with the cursor since we can assign and return multiple variables simultaneously.
+    # In the stage of setting up the connection with the DB, sometimes we may encounter different exceptions.
+    # In practice, we handle this crucial stage carefully by using try/except block.
     try:
-        conn = psycopg2.connect('dbname="tournament" user="dbuser" host="localhost" password="dbpass"')
+        db = psycopg2.connect("dbname={}".format(database_name))
+        cursor = db.cursor()
+        return db, cursor
     except:
-        print "Connect to the database"
+        raise IOError("Connected!\n")
     
 def deleteMatches():
     """Remove all the match records from the database."""
-    conn = connect()
-    query = "TRUNCATE matches"
-    cur.execute(query)
-    conn.commit()
-    conn.close()
+    db, cursor = connect()
+    cursor = db.cursor()
+    
+    query = "TRUNCATE matches;"
+    cursor.execute(query)
+
+    db.commit()
+    db.close()
 
 def deletePlayers():
     """Remove all the player records from the database."""
-    conn = connect()
-    query = "DELETE FROM players"
-    cur.execute(query)
-    conn.commit()
-    conn.close()
+    query = "DELETE FROM players;"
+    cursor.execute(query)
+
+    db.commit()
+    db.close()
 
 def countPlayers():
     """Returns the number of players currently registered."""
-    conn = connect()
-    query = "SELECT count(*) AS num FROM players"
-    cur.execute(query)
-    players_count = c.fetchone()[0]
-    conn.commit()
-    conn.close()
+    query = "SELECT count(*) AS num FROM players;"
+    cursor.execute(query)
+    count = cursor.fetchone()[0]
+
+    db.close()
     return count
 
 def registerPlayer(name):
@@ -52,13 +59,13 @@ def registerPlayer(name):
     Args:
     name: the player's full name (need not be unique).
     """
-    conn = connect()
     query = "INSERT INTO players (name) VALUES (%s);"
     parameter = (name,)
-    cur.execute = (query, parameter)
-    conn.commit()
-    conn.close()
+    cursor.execute(query, parameter)
 
+    db.commit()
+    db.close()
+    
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
     The first entry in the list should be the player in first place, or a player
@@ -71,17 +78,11 @@ def playerStandings():
     wins: the number of matches the player has won
     matches: the number of matches the player has played
     """
-    conn = connect()
-    query = ("SELECT id, name, COUNT(matches.winner) AS wins, "
-             "(SELECT games FROM games_view WHERE games_view.id = players.id) "
-             "FROM players LEFT JOIN matches "
-             "ON players.id = matches.winner "
-             "GROUP BY players.id, players.name "
-             "ORDER BY wins DESC")
-    cur.execute(query)
-    playerStandings = c.fetchall() #Fetches all remaining rows of a query result, returning a list.
-    conn.close()
-    return playerStandings
+    cursor.execute("SELECT * FROM standings ORDER BY wins DESC;")
+    playerslist = cursor.fetchall() # Fetches all remaining rows of a query result, returning a list.
+
+    db.close()
+    return playerslist
 
 def reportMatch(winner, loser):
     """Records the outcome of a single match between two players.
@@ -89,12 +90,12 @@ def reportMatch(winner, loser):
     winner: the id number of the player who won
     loser: the id number of the player who lost
     """
-    conn = connect()
-    query = "INSERT INTO matches (winner_id, loser_id) VALUES (%s, %s)"
+    query = "INSERT INTO matches (winner, loser) VALUES (%s, %s);"
     parameter = (winner, loser)
-    cur.execute(query, parameter)
-    conn.commit()
-    conn.close()
+    cursor.execute(query, parameter)
+
+    db.commit()
+    db.close()
 
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
@@ -105,20 +106,25 @@ def swissPairings():
     to him or her in the standings.
   
     Returns:
-      A list of tuples, each of which contains (id1, name1, id2, name2first player's unique id
-        name1: the first player's name
-        id2: the second player's unique id
-        name2: the second player's name
+      A list of tuples, each of which contains (id1, name1, id2, name2),
+      first player's unique id
+      name1: the first player's name
+      id2: the second player's unique id
+      name2: the second player's name
     """
-    # For pairingsiterator relied on the recipes section of Python's
+    # For swissPairings consulted GitHub, Stack OverFlow
+    # and the recipes section of Python's
     # itertools docs: https://docs.python.org/2/library/itertools.html
-    # and the Python Standard Library
+    # and the Python Standard Library.
 
-    # Iterate through the list and build the pairings
-  
-    standings = playerStandings()
-    pairingsiterator = itertools.izip(*[iter(standings)]*2)
+    # Iterate through the list and build the pairings to return results
     results = []
+    pair = []
+    standings = playerStandings()
+    # standings = [(id1, name1, wins1, matches1), (id2, name2, wins2, matches2)]
+    # [id1, id2, id3, id4, id5, id6, id7, id8] = [row[0] for row in standings]
+    # pairings = swissPairings()
+    pairingsiterator = itertools.izip(*[iter(standings)]*2)
     pairings = list(pairingsiterator)
     for pair in pairings:
         id1 = pair[0][0]
